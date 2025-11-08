9b4854859a3f141119039f7dff01f0e853db4e32
Dar3st <d2rk3st@gmail.com>
2025-11-08 22:38:40 +0300

    Added logic equipment slot and added items

--- src/main/java/EquipmentSlot.java
+++ src/main/java/EquipmentSlot.java
@@ -5,9 +5,11 @@ public enum EquipmentSlot {
     CHEST("Грудь"),
     HANDS("Руки"),
     LEGS("Ноги"),
-    FEET("Ступни"),
+    FEET("Ботинки"),
     RING_1("Кольцо 1"),
     RING_2("Кольцо 2"),
+    RING_3("Кольцо 3"),
+    RING_4("Кольцо 4"),
     NECK("Шея"),
     BELT("Пояс");
 
